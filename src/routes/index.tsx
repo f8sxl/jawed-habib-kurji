cf0d05e26f8487b43d4a1ff77c919bb0a914ee39
f8sxl <f8sxlq@gmail.com>
2026-07-20 15:48:58 +0530

    fix: add top padding to hero to prevent navbar collision

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1107,7 +1107,7 @@ function Hero() {
 
       <motion.div
         style={{ opacity }}
-        className="relative z-10 mx-auto flex max-w-7xl flex-col gap-8 px-6 pb-24 pt-[50svh] lg:h-full lg:min-h-0 lg:flex-row lg:items-end lg:justify-between lg:gap-12 lg:pb-28 lg:pt-0"
+        className="relative z-10 mx-auto flex max-w-7xl flex-col gap-8 px-6 pb-24 pt-28 lg:h-full lg:min-h-0 lg:flex-col lg:justify-end lg:gap-12 lg:pb-28 lg:pt-36"
       >
         <div className="flex flex-col">
           <motion.div
